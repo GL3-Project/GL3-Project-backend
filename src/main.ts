f1c87120a8d0edb47565a4ba790bedd97afc04fa
Habib Sellami <habib.sellami@outlook.com>
2023-04-01 05:59:22 +0100

    Testing pipelines

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   console.log('Listening on port', process.env.PORT || 8080, '...');
   await app.listen(process.env.PORT || 8080);
+
 }
 bootstrap();
