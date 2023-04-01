08b5f169438538abbce6457fac066fd5f55d4c52
Habib Sellami <habib.sellami@outlook.com>
2023-04-01 03:03:32 +0100

    Change return value to test the new pipeline

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World from main!');
+      expect(appController.getHello()).toBe('Hello World from main with! This is a dockerized build.');
     });
   });
 });
