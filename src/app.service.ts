2ec7231e0f94d871a1b6d31bbfa3eafa838b2aa4
Habib Sellami <habib.sellami@outlook.com>
2023-04-08 01:06:56 +0100

    Modify pipeline to deploy on vm

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World from main with! This is a dockerized build.';
+    return 'Hello World from main with! This is a dockerized build. testing pipelines';
   }
 }
