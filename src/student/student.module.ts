540d5d07011838a6cff4533a1120ec773b66a8da
Habib Sellami <habib.sellami@outlook.com>
2023-05-28 18:14:21 +0100

    Fix dependency injection issues

--- src/student/student.module.ts
+++ src/student/student.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { StudentService } from './student.service';
 import { StudentController } from './student.controller';
+import { Repository } from 'typeorm';
+import { Student } from '@student/entities/student.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
 	controllers: [StudentController],
-	providers: [StudentService],
+	providers: [StudentService, Repository<Student>],
+	exports: [StudentService],
+	imports: [TypeOrmModule.forFeature([Student])],
 })
 export class StudentModule {}
--- src/template/template.module.ts
+++ src/template/template.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TemplateService } from './template.service';
 import { TemplateController } from './template.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Template } from '@template/entities/template.entity';
 
 @Module({
 	controllers: [TemplateController],
 	providers: [TemplateService],
+	imports: [TypeOrmModule.forFeature([Template])],
 })
 export class TemplateModule {}
